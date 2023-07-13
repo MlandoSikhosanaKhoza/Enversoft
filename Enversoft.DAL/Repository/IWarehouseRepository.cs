929688b55beb933f49b9e1c57aa252fab3979588
mlando2.0 <mlando2.0@outlook.com>
2023-07-13 02:00:19 +0200

    Warehouse CRUD

---
+++ Enversoft.DAL/Repository/IWarehouseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Enversoft.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft.DAL
+{
+    public interface IWarehouseRepository
+    {
+        List<Warehouse> GetWarehouses();
+    }
+}
---
+++ Enversoft.DAL/Repository/WarehouseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Enversoft.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft.DAL
+{
+    public class WarehouseRepository : IWarehouseRepository
+    {
+        private readonly IGenericRepository<Warehouse> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WarehouseRepository(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
+            _repository = unitOfWork.GetRepository<Warehouse>();
+        }
+        
+        public List<Warehouse> GetWarehouses()
+        {
+            return _repository.Get().ToList();
+        }
+
+        public bool DeleteWarehouse(Warehouse WarehouseObject) { 
+            _repository.Delete(WarehouseObject);
+            _unitOfWork.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateWarehouse(Warehouse WarehouseObject)
+        {
+            _repository.Update(WarehouseObject);
+            _unitOfWork.SaveChanges();
+            return true;
+        }
+
+        public Warehouse AddWarehouse(Warehouse WarehouseObject)
+        {
+            Warehouse WarehouseAdded=_repository.Insert(WarehouseObject);
+            _unitOfWork.SaveChanges();
+            return WarehouseAdded;
+        }
+
+
+    }
+}
