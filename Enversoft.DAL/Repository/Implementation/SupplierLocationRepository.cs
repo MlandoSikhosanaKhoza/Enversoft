77a46ca65391c9bdc13f5d433c575484ca8363a1
mlando2.0 <mlando2.0@outlook.com>
2023-07-13 03:07:59 +0200

    Error fix

---
+++ Enversoft.DAL/Repository/Implementation/SupplierLocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft.DAL.Repository.Implementation
+{
+    internal class SupplierLocationRepository
+    {
+    }
+}
